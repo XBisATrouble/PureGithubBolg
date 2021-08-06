2ca65a7f26d2afad9bb8a873ed1290c80368eb3f
xbisatrouble <xbisatrouble@gmail.com>
2021-08-06 11:47:23 +0800

    update

--- pure.py
+++ pure.py
@@ -122,7 +122,7 @@ jinja_env = Environment(loader=PackageLoader(__name__))
 website_dir = "../XBisATrouble.github.io"
 
 # 博客名字
-jinja_env.globals["title"] = "Blog"
+jinja_env.globals["title"] = "博客"
 
 # 博客图标
 jinja_env.globals["icon"] = "logo.bmp"
